2eef9ee4130cdff5cf7c2ae7747a52293202b0b8
kshen47 <katherineshen@gatech.edu>
2022-06-20 17:22:12 -0400

    adding prev updates lol i forgot to push them

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>hello, i'm kat.</h1>
+
+            <p>i’m a computer science major studying at georgia tech with a passion for ui/ux, product design, data science, and more.</p>
+            <p>download my resume here</p>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>kws</h1>
+        </div>
+    );
+}
+ 
+export default Navbar;
