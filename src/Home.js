a7b612837d93cdf71474606f71b7f746ce9f52f4
kshen47 <katherineshen@gatech.edu>
2022-07-27 22:00:37 -0400

    change resume link

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
             </h1>
 
             <p>i’m a computer science major studying at georgia tech with a passion for ui/ux, product design, data science, and more.</p>
-            <p>download my resume <a href='./resume.pdf' download>here</a></p>
+            <p>access my resume <a href='https://github.com/shenkw1/resume/blob/main/resume.pdf' download>here</a></p>
         </div>
     );
 }
